63cef8bd909d33b0ba474dbdc06638f72f9f35da
Tyler <tyscott039@gmail.com>
2026-06-07 18:16:02 -0400

    we created an additional ingress rule to allow traffic from our docker application on port 8080

--- modules/secgroups/main.tf
+++ modules/secgroups/main.tf
@@ -14,7 +14,13 @@ resource "aws_security_group" "nextwork_devops_sg" {
         protocol = "tcp"
         cidr_blocks = [var.my_ip_address]
     }
-
+    ingress {
+        description = "allows inbound traffic from our docker application"
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress { 
         description = "allows all outbound traffic"
         from_port = 0
